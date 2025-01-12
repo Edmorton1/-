fa1a3d4787004d0b68f9fa16a01fef6903661d0c
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-01-12 22:32:16 +0500

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import getRomanji from "./Files/getRomanji"
 import { arr } from "./Files/getRomanji"
+import Music from "./Files/music"
 
 function App() {
   const [minKanji, setMinKanjii] = useState(2)
@@ -20,41 +21,40 @@ function App() {
     func(e)
    }
   }
-  //
+
   function randomazeGif() {
     setTimeout(() => {
-      document.body.style.backgroundImage = `url(${arr[0]})`
+      document.body.style.backgroundImage = `url(${arr[1]})`
     })
     setTimeout(() => {
-      document.body.style.backgroundImage = `url(${arr[1]})`
-    }, 5000)
-    setTimeout(() => {
       document.body.style.backgroundImage = `url(${arr[2]})`
-    }, 10000)
+    }, 3000)
+    setTimeout(() => {
+      document.body.style.backgroundImage = `url(${arr[0]})`
+    }, 6000)
   }
-  randomazeGif()
   setInterval(() => {
     randomazeGif()
-  }, 15000)
-  
-  //
+  }, 9000)
+
   return (
     <>
       <div className="content">
-        <div className="title">♡ Генератор ников для анимешников ♡</div>
+        <Music />
+        <div className="title">♥ Генератор ников для анимешников ♥</div>
         <div className="nick">{nick}</div>
           <form>
             кол-во иероглифов:
-            от <input type="number" style={{marginRight: "5px"}} min={1} onChange={(e) => {handleInput(Number(e.target.value), setMinKanjii)}} max={46} value={minKanji} />
-            до <input type="number" min={1} onChange={(e) => {handleInput(Number(e.target.value), setMaxKanji)}} max={46} value={maxKanji}/>
+            от <input type="number" style={{marginRight: "5px"}} min={1} onChange={(e) => {handleInput(Number(e.target.value), setMinKanjii)}} max={46} placeholder="2" />
+            до <input type="number" min={1} onChange={(e) => {handleInput(Number(e.target.value), setMaxKanji)}} max={46} placeholder="5"/>
           </form>
-          
         <div className="gif"><img src="https://i.giphy.com/tXwHTbQuyjo1q.webp"/></div>
-        <button onClick={() => (setNick(`${getRomanji(minKanji, maxKanji)[0]}${getRomanji(minKanji, maxKanji)[1]}`))}>СГЕНЕРИРОВАТЬ</button>
+        <button onClick={() => (setNick(`${getRomanji(minKanji, maxKanji)[0]}${getRomanji(minKanji, maxKanji)[1]}`))} className="button">СГЕНЕРИРОВАТЬ</button>
       </div>
-      <footer>Сделал <a href="https://github.com/Edmorton1" target="_blank">Edmorton</a></footer>
+      <footer>Автор <a href="https://github.com/Edmorton1" target="_blank">Edmorton</a></footer>
     </>
   )
 }
 
 export default App
+// https://dl2.mp3party.net/online/10471296.mp3
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./Files/App.scss"
+import "./Files/appMedia.scss"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
